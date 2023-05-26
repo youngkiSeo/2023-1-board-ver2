0a67c2f89be44e70a8e31899b87dea1621c13fff
서영기 <matep8@knou.ac.kr>
2023-05-26 11:14:59 +0900

    first

---
+++ boardver2/src/main/java/com/green/boardver2/Boardver2Application.java
@@ -0,0 +1,13 @@
+package com.green.boardver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boardver2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boardver2Application.class, args);
+    }
+
+}
---
+++ boardver2/src/test/java/com/green/boardver2/Boardver2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.boardver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boardver2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
